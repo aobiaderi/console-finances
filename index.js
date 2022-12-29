5f9e24ccf67e9742ba372268b41a615359cf1799
Obee123 <aobiaderi@yahoo.com>
2022-12-29 18:50:12 +0000

    updated calc and images to previous solution

--- index.js
+++ index.js
@@ -224,8 +224,7 @@ for (var i = 1; i < finances.length; i++) {
   previousAmount = currentAmount;
 //   console.log("new previous amount which was the current amt entry: " + previousAmount);
 }
-// Divide by 85 months instead of 86 due to comparison being between 2 months
-var averageChange = totalChange / (finances.length- 1);
+var averageChange = totalChange / (finances.length);
 averageChange = averageChange.toFixed(2);
 console.log("Average Change: " +"$" + averageChange);
 
